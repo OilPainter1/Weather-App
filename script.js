44db205ae6b1c4e9a0ddad50885b0bc61f969b83
Henry Cryns <henrycryns@MacBook-Air.local>
2022-10-05 10:08:18 -0500

    Weather API fetch coordinates started

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 var APIKey = "b0dae24107c6909617e7f4fbbd653e80"
+
+var searchBtn = document.getElementById("searchButton")
+
+function handleSearch(){
+    var searchedWeather= document.querySelector("input").value
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=Madison&appid=" + APIKey)
+    .then(function(response){
+        console.log(response.json())
+
+    })
+    
+    
+}
+
+searchBtn.addEventListener("click",handleSearch)
