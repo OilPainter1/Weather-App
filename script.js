6a1f7a5438a2f31b98687eba166b4064b35276cf
Henry Cryns <henrycryns@MacBook-Air.local>
2022-10-06 16:38:13 -0500

    fixed bug which occurs with multiple searches without page refresh

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var searchBtn = document.getElementById("searchButton")
 
 function handleSearch(){
     var searchedWeather= document.querySelector("input").value
+    removeWeatherForecastBlocks()
+    
     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchedWeather +"&appid=" + APIKey)
         .then(function(response){
             var jsonData= response.json()
@@ -36,9 +38,11 @@ function loadWeatherForecast(data){
     console.log(data)
     for(var i=0;i<40;i=i+8){
         var weatherDayForecast= document.createElement("div")
-        var WeatherBarDiv=document.getElementById("weatherBar")
+        var WeatherBarDiv=document.getElementById("weatherDisplay")
         WeatherBarDiv.appendChild(weatherDayForecast)
-        weatherDayForecast.textContent=moment.unix(data.list[i].dt).format("MMMM Do YYYY, h:mm a")
+        
+
+        weatherDayForecast.textContent=data.city.name +": " + moment.unix(data.list[i].dt).format("MMMM Do YYYY, h:mm a") +" Temp: " + data.list[i].main.temp
         weatherDayForecast.style.padding="10px"
         weatherDayForecast.style.margin="5px"
         weatherDayForecast.style.backgroundColor="Linen"
@@ -48,6 +52,17 @@ function loadWeatherForecast(data){
     }
 }
 
+
+function removeWeatherForecastBlocks(){
+    if(document.getElementById("weatherDisplay").firstChild!=null){
+        for(var i= 0;i<5;i++){
+            document.getElementById("weatherDisplay").removeChild(document.getElementById("weatherDisplay").firstChild)
+        }
+    } else{
+        return
+    }
+
+}
 
 
 
