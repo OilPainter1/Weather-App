fd0cf0f3434e15cfd6674c189f5eb1209feec151
Henry Cryns <henrycryns@MacBook-Air.local>
2022-10-06 11:43:04 -0500

    fetch weather implemented

--- script.js
+++ script.js
@@ -4,13 +4,36 @@ var searchBtn = document.getElementById("searchButton")
 
 function handleSearch(){
     var searchedWeather= document.querySelector("input").value
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=Madison&appid=" + APIKey)
-    .then(function(response){
-        console.log(response.json())
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchedWeather +"&appid=" + APIKey)
+        .then(function(response){
+            var jsonData= response.json()
+            return jsonData
+            
+        })
+        .then(function(data){
+            
+            var lat = data[0].lat
+            var lon = data[0].lon
+            fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat +"&lon=" + lon + "&appid=" + APIKey + "&units=imperial")
+            .then(function(response){
+               jsonData=response.json()
+               return jsonData
+            })
+            .then(function(data){
+                console.log(data)
+            })
+           
 
-    })
+            
+
+        })
+      
     
-    
+   
 }
+
+
+
+
 
 searchBtn.addEventListener("click",handleSearch)
